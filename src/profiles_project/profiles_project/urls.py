caf4512c93dfba674054a68655902a2cd014204e
Gene Romuga <generomuga12693@gmail.com>
2017-12-03 09:04:26 +0800

    removed old requirement.tx

--- src/profiles_project/profiles_project/urls.py
+++ src/profiles_project/profiles_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+"""used to call the urls.py from the app"""
+from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('profiles_api.urls')),
 ]
